88fdb14affa043b59b8f97bf68a72b4ec150596a
Sreevidhya <vidhyareddem@gmail.com>
2023-04-08 11:57:33 -0400

    made changes in app and index folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Route, Router, Routes } from 'react-router-dom';
+
 import './App.css';
 import './index.css';
 // import './style.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
 
 import React from 'react';
 
@@ -15,12 +17,17 @@ import MainIndex from "./components/MainIndex.js";
 import Home from "./components/Home.js";
 import MenuList from "./components/MenuList.js";
 import Login from './components/Login';
+import MenuItems from './components/MenuItems';
 
 
 function App() {
   return (
    <React.Fragment>
-     <Home/>
+     <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/menuItems' element={<MenuItems/>}/>          
+        </Routes>
    
    </React.Fragment>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
 
 import 'mdbootstrap/css/bootstrap.css';
 import 'mdbootstrap/css/bootstrap.min.css';
@@ -10,9 +12,11 @@ import '@fortawesome/fontawesome-free/css/fontawesome.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 
