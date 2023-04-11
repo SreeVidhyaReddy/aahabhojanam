353fb5dd1c2386d12ff4b6832ae74a95d566da5f
Sreevidhya <vidhyareddem@gmail.com>
2023-04-10 21:23:29 -0400

    login page styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 
 class Login extends React.Component{
 	render(){
+        
 		return(
             <React.Fragment>
            <div className="container-logo" id="container-logo">
@@ -15,35 +16,36 @@ class Login extends React.Component{
             </div>
             <div>
                     <div className="form-container sign-up-container">
-                            <form action="#">
-                                <input type="email" placeholder="Email" id="email" />
-                                <span id="error-email"></span>
-                                <input type="password" placeholder="Password" id="password"/>
-                                <span id="error-password"></span>
+                            <form className='form' action="#">
+                                <input className='input' type="email" placeholder="Email" id="email" />
+                                <span className='span' id="error-email"></span>
+                                <input className='input'type="password" placeholder="Password" id="password"/>
+                                <span className='span' id="error-password"></span>
                                 <button id="register" onclick="store()">Sign Up</button>
+                                
                             </form>
                         </div>
                         <div className="form-container sign-in-container">
-                            <form action="index.html">
+                            <form className='form' action="#">
 
-                                <input type="email" placeholder="Email" id="login-email" />
-                                <span id="login-error-email"></span>
-                                <input type="password" placeholder="Password" id="login-password"/>
-                                <span id="login-error-password"></span>
-                                <Link to="/MainIndex" id="register" >Login</Link>
+                                <input className='input' type="email" placeholder="Email" id="login-email" />
+                                <span className='span' id="login-error-email"></span>
+                                <input className='input' type="password" placeholder="Password" id="login-password"/>
+                                <span className='span' id="login-error-password"></span>
+                                <Link to="/MainIndex" id="register" >sign In</Link>
 
                             </form>
                         </div>
                         <div className="overlay-container">
                             <div className="overlay">
                                 <div className="overlay-panel overlay-left">
-                                    <h1>Welcome Back!</h1>
-                                    <p>To keep connected with us please login with your personal info</p>
+                                    <h1 className='h1'>Welcome Back!</h1>
+                                    <p className='p'>To keep connected with us please login with your personal info</p>
                                     <button className="ghost" id="signIn" onclick="check()">Sign In</button>
                                 </div>
                                 <div className="overlay-panel overlay-right">
-                                    <h1>Hello, Foodies!</h1>
-                                    <p>Enjoy a taste of heaven!</p>
+                                    <h1 className='h1'>Hello, Foodies!</h1>
+                                    <p className='p'>Enjoy a taste of heaven!</p>
                                     <button className="ghost" id="signUp" >Sign Up</button>
                                 </div>
                             </div>
